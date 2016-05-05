0766a1082e6dcf68da8ca3e284ae23c5f16e766c
Baljeet <baljeet@ameotech.com>
2016-05-05 13:26:44 +0530

    For heroku deployment.

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path')
+var fs = require('fs');
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200'
+  //log: 'trace'
+});
+
+var app = module.exports = express();
+app.use(require('prerender-node').set('prerenderToken', 'u1qUx4lJfTyXfD5vp4t5')); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+var usersFilePath = path.join(__dirname, 'data.json');
+// This will ensure that all routing is handed over to AngularJS 
+app.get('/', function(req, res){ 
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') }); 
+});
+
+app.get('/api/getmeds',function(req,res){	
+	//var readable = fs.createReadStream(usersFilePath);
+	//readable.pipe(res)	
+	client.search({
+		  index: 'cp',
+		  type:'sections',
+          size:1000
+		  //q: '*'
+		}, function (error, response) {
+			var hits = response.hits.hits;
+			var hitsRes = [];
+			console.log(hits.length);
+			for( var index=0;index<=hits.length-1;index++)
+			{
+				var hit = hits[index];
+				hitsRes.push(hit._source);
+			}
+			res.send(JSON.stringify(hitsRes));
+		});
+});
+
+app.listen(8081); 
+console.log('App is running on port : 8081')
---
+++ public/app.js
@@ -0,0 +1,64 @@
+// app.js
+
+var app = angular.module('dm-app', ['ngRoute'])
+
+.config(function ($routeProvider, $locationProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'views/homeView.html',
+        controller: 'homeController'
+    })
+
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix('!');
+});
+
+
+app.service('dailyMedService', ['$http', function ($http) {
+    var dailyMedService = {
+        getDailyMeds: function () {
+            return $http.get('http://localhost:8081/api/getmeds');
+        }
+    };
+    return dailyMedService;
+}]);
+
+function mainController($scope) {
+    // We will create an seo variable on the scope and decide which fields we want to populate 
+    $scope.seo = {
+        pageTitle: '', pageDescription: ''
+    };
+}
+
+function homeController($scope, dailyMedService, $anchorScroll, $location, $window) {
+    $scope.getAllMeds = function () {
+        dailyMedService.getDailyMeds()
+            .then(function (response) {
+                $scope.medicines = response.data;                
+            });
+    };
+
+    $scope.gotoAnchor = function (x) {       
+        var newHash = '#d' + x;
+        var p = $(newHash);
+        var offset = p.offset();        
+        $('html, body').animate({
+            scrollTop: offset.top-30
+        }, 500);       
+    };
+}
+
+function aboutController($scope) {
+    $scope.$parent.seo = {
+        pageTitle: 'About',
+        pageDescripton: 'We are a content heavy website so we need to be indexed.'
+    };
+}
+
+function featuresController($scope) {
+    $scope.$parent.seo = { pageTitle: 'Features', pageDescripton: 'Check out some of our awesome features!' };
+}
